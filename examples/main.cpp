949b4821d4f89b8aad397a092754cae8c737a057
artibarti <safranyartur1995@gmail.com>
2018-11-07 11:35:17 +0100

    add some example textures

--- examples/main.cpp
+++ examples/main.cpp
@@ -18,11 +18,24 @@ int main(int argc, char** argv)
     GLCube cube6 = GLCube(glm::vec3(-3,0,0), 1.0);
     GLCube cube7 = GLCube(glm::vec3(2,0,-2), 1.0);
     GLCube cube8 = GLCube(glm::vec3(3,0,-2), 1.0);
+    
+    GLCube cube9 = GLCube(glm::vec3(-7,0,-2), 1.0);
+    GLCube cube10 = GLCube(glm::vec3(-7,1,-2), 1.0);
+    GLCube cube11 = GLCube(glm::vec3(-9,0,-2), 1.0);
+    GLCube cube12 = GLCube(glm::vec3(-7,2,-2), 1.0);
+    GLCube cube13 = GLCube(glm::vec3(-7,3,-2), 1.0);
+    GLCube cube14 = GLCube(glm::vec3(-9,1,-2), 1.0);
+    GLCube cube15 = GLCube(glm::vec3(-9,2,-2), 1.0);
+    GLCube cube16 = GLCube(glm::vec3(-9,3,-2), 1.0);
+    GLCube cube17 = GLCube(glm::vec3(-8,3,-2), 1.0);
+
     GLCube floor = GLCube(glm::vec3(0,-15,0), 30.0);
+
 
     cubes = 
     {
-        &cube1, &cube2, &cube3, &cube4, &cube5, &cube6, &cube7, &cube8
+        &cube1, &cube2, &cube3, &cube4, &cube5, &cube6, &cube7, &cube8,
+        &cube9, &cube10, &cube11, &cube12, &cube13, &cube14, &cube15, &cube16, &cube17
     };
 
     GLScene scene = GLScene(640,480);
@@ -36,7 +49,7 @@ int main(int argc, char** argv)
         scene.attachDrawable(cube);
     }
 
-    GLTexture chess_texture("textures/chessboard.jpg");    
+    GLTexture chess_texture("textures/grass2.jpg");    
     floor.attachTexture(&chess_texture);
     scene.attachDrawable(&floor);
 
